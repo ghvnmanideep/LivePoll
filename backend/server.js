73f1cb26c7a6a09438a50443435edf2890c56c24
Gattupalli Hema Venkata Naga Mani Deep <ghvnmanideep@gmail.com>
2025-08-08 15:34:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(()=>console.log("MongoDB connected"))
-  .catch(err=>console.error("MongoDB error:", err));
+// Connect to MongoDB without deprecated options
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/polls", require("./routes/polls"));
